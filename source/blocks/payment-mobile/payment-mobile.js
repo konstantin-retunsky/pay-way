0554b47225f094b2ba1db08827805605bca2d97f
Konstantin Retunsky <konstantin.retunsky@gmail.com>
2021-06-23 15:36:12 +0500

    block done

--- source/blocks/payment-mobile/payment-mobile.js
+++ source/blocks/payment-mobile/payment-mobile.js
@@ -1,1 +1,34 @@
+const buttonsPaymentMobile = document.querySelectorAll(
+	".payment-mobile__switcher-button"
+);
 
+if (buttonsPaymentMobile) {
+	buttonsPaymentMobile.forEach((item) => {
+		item.addEventListener("click", function (e) {
+			const fisrtElement = document.querySelectorAll(
+				".payment-mobile__switcher-button"
+			)[0];
+			const cloneFirstElement = fisrtElement.cloneNode(true);
+
+			fisrtElement.innerHTML = this.innerHTML;
+			this.innerHTML = cloneFirstElement.innerHTML;
+		});
+	});
+}
+
+const paymentMobileSwitcher = document.querySelector(
+	".payment-mobile__switcher-list"
+);
+const paymentMobileWrapper = document.querySelector(
+	".payment-mobile__switcher-list-wrapper"
+);
+
+if (paymentMobileSwitcher && paymentMobileWrapper) {
+	paymentMobileSwitcher.addEventListener("mouseenter", function (e) {
+		paymentMobileWrapper.style.width = `${this.offsetWidth}px`;
+	});
+
+	paymentMobileSwitcher.addEventListener("mouseleave", function (e) {
+		paymentMobileWrapper.style.width = "auto";
+	});
+}
